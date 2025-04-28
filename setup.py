d6a62fcab7133734ed06775f8d27dd62f59555ad
Parker Owan <parker.owan@gmail.com>
2025-04-28 13:05:08 -0700

    Changes the pip protobuf generation to work with grpc_tools

--- setup.py
+++ setup.py
@@ -3,17 +3,48 @@
 
 import os
 from typing import List
-from setuptools import setup, find_packages
-from setuptools_protobuf import Protobuf
+from setuptools import Command, setup, find_packages
+from setuptools.command.build import build
+import subprocess
 
 
-def find_protobufs() -> List[Protobuf]:
-    proto_dir = "rtix/api"
-    protos = []
-    for file in os.listdir(proto_dir):
-        if file.endswith(".proto"):
-            protos.append(Protobuf(os.path.join(proto_dir, file)))
-    return protos
+class GenerateProtos(Command):
+    """
+    Generate protos.  The usual protobuf-distutils uses protoc, which can
+    generate older/incompatible protos from the grpc_tools.protoc version.
+    """
+    user_options = []
+
+    def initialize_options(self) -> None:
+        pass
+
+    def finalize_options(self) -> None:
+        pass
+
+    def run(self):
+        protos = self.find_protobufs()
+        for proto in protos:
+            protoc_call = [
+                "python3",
+                "-m",
+                "grpc_tools.protoc",
+                "--proto_path=rtix/api",
+                "--python_out=rtix/api",
+                proto,
+            ]
+            subprocess.call(protoc_call)
+
+    def find_protobufs(self) -> List[str]:
+        proto_dir = "rtix/api"
+        protos = []
+        for file in os.listdir(proto_dir):
+            if file.endswith(".proto"):
+                protos.append(file)
+        return protos
+
+
+class CustomBuild(build):
+    sub_commands = [('generate_protos', None)] + build.sub_commands
 
 
 # Generate the bindings
@@ -26,8 +57,10 @@ setup(
     "Fast and lightweight IPC and orchestration layer for robotics and embodied AI applications.",
     packages=find_packages(include=["rtix", "rtix.*"]),
     package_data={"rtix.api": ["*.proto"]},
-    setup_requires=["setuptools-protobuf"],
-    protobufs=find_protobufs(),
+    cmdclass={
+        "build": CustomBuild,
+        "generate_protos": GenerateProtos
+    },
     install_requires=[
         "grpcio>=1.70.0",
         "grpcio-tools>=1.70.0",
